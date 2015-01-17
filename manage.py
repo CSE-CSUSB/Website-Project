ac91d44e01e6d05cb3333da99ae84b6a47f8a458
Mike Korcha <mikekorcha@gmail.com>
2015-01-17 12:56:44 -0800

    added migration script that kinda half works?

---
+++ manage.py
@@ -0,0 +1,11 @@
+from loader import app, db
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
